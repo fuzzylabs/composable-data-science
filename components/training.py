0390449c0e5c220c726b51853f72c82c8708d406
archena <archena@users.noreply.github.com>
2019-11-11 17:45:48 +0000

    Dependencies in Python components

--- components/training.py
+++ components/training.py
@@ -1,3 +1,6 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
 def fit(train_X, train_y):
     rf_model = RandomForestRegressor(random_state=1, n_estimators=10)
     rf_model.fit(train_X, train_y)
@@ -5,4 +8,4 @@ def fit(train_X, train_y):
 
 def validate(model, val_X, val_y):
     rf_val_predictions = model.predict(val_X)
-    rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+    return mean_absolute_error(rf_val_predictions, val_y)
